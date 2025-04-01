ed88986cae5ef5bd7eb9abb7cf3ea0381d327f47
Dilan <dilanmasushanka533@gmail.com>
2025-04-01 18:23:49 +0530

    Replaced 999

--- src/pages/index.js
+++ src/pages/index.js
@@ -364,7 +364,7 @@ export default function HomePage() {
                 alt: "Top New Travel Credit Card Offers of 2025",
                 title: "Top New Travel Credit Card Offers of 2025",
                 desc: "Breaking news on the latest card launches and exclusive sign-up bonuses.",
-                link: "/reviews/top-new-travel-credit-card-offers-2025"
+                link: "/reviews/top-new-travel-credit-card-offers-2025",
               }
               
               {
