7a6b2a7b866812562f54bcbe3251399de0f0b56c
Dilan <dilanmasushanka533@gmail.com>
2025-04-01 19:52:54 +0530

    Replaced st1655

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
 import "../styles/globals.css";
-import "../styles/reveiws 2025.css";
+import "../styles/reviews 2025.css";
 import Header from "../components/Header"; // ✅ Import the Header component
 
 function MyApp({ Component, pageProps }) {
