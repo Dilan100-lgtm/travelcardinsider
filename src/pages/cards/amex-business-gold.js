867a7ac38ab1c3daf3b37b0676d6f91456d35c4e
Dilan <dilanmasushanka533@gmail.com>
2025-04-23 09:55:15 +0530

    cdccd

--- src/pages/cards/amex-business-gold.js
+++ src/pages/cards/amex-business-gold.js
@@ -206,7 +206,7 @@ function AmexBusinessGoldReviewPage() {
                          <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
                        </svg>
                     </button>
-                    TCI Rating (Hypothetical): <strong>{reviewData.ratingValue.toFixed(1)}</strong>/10
+                    TCI Rating : <strong>{reviewData.ratingValue.toFixed(1)}</strong>/10
 
                     {/* --- Conditionally Rendered Tooltip --- */}
                     {showRatingInfo && (
