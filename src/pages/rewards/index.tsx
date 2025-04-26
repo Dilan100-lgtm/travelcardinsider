cad281ac3ff789ed3b94873f1f4faa727f7221d4
Dilan <dilanmasushanka533@gmail.com>
2025-04-26 17:03:27 +0530

    kjnsckjnd

--- src/pages/rewards/index.tsx
+++ src/pages/rewards/index.tsx
@@ -26,8 +26,6 @@ export default function RewardsPage() {
 
       <main className={styles.calculatorContainer}>
         
-          
-       
 
         <RewardsCalculator />
         <small className={styles.heroNote}>
