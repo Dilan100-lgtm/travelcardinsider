eab8d201f857ee747d6f4782a2ccd5223f138f86
Dilan <dilanmasushanka533@gmail.com>
2025-04-27 16:13:43 +0530

    klfmvjf

--- src/pages/index.js
+++ src/pages/index.js
@@ -64,9 +64,9 @@ export default function HomePage() {
 
         {/* Preload critical fonts */}
         <link rel="preload" href="/fonts/Roboto_Condensed-Regular.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
-        <link rel="preload" href="/fonts/Roboto_Condensed-bold.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
-        <link rel="preload" href="/fonts/playfair-display-regular.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
-        <link rel="preload" href="/fonts/playfair-display-bold.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
+        <link rel="preload" href="/fonts/Roboto_Condensed-Bold.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
+        <link rel="preload" href="/fonts/PlayfairDisplay-Regular.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
+        <link rel="preload" href="/fonts/Playfair-Display-Bold.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
 
         <script type="application/ld+json" dangerouslySetInnerHTML={{
           __html: JSON.stringify({
