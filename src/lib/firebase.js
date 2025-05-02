fc31c507bb8be17feb9af25164bc9c3d34454ad8
Dilan <dilanmasushanka533@gmail.com>
2025-05-02 12:16:13 +0530

    5564

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,18 +1,21 @@
-// lib/firebase.js
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'YOUR_API_KEY',
-  authDomain: 'YOUR_PROJECT_ID.firebaseapp.com',
-  projectId: 'YOUR_PROJECT_ID',
-  storageBucket: 'YOUR_PROJECT_ID.appspot.com',
-  messagingSenderId: 'YOUR_SENDER_ID',
-  appId: 'YOUR_APP_ID',
+  apiKey: "AIzaSyDXlV4US6QfgIh7Y7j2uJ-r6dxqoad1dEk",
+  authDomain: "travelcardinsider.firebaseapp.com",
+  projectId: "travelcardinsider",
+  storageBucket: "travelcardinsider.firebasestorage.app",
+  messagingSenderId: "149061612901",
+  appId: "1:149061612901:web:d626ee8689f7da6e685d8c",
+  measurementId: "G-V3S6KKPXBB"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
