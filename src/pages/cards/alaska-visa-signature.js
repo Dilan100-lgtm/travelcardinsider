b21525f037a3b4c19ed5c10c389b850d1bdbeefd
Dilan <dilanmasushanka533@gmail.com>
2025-05-28 10:36:10 +0530

    klcfj

--- src/pages/cards/alaska-visa-signature.js
+++ src/pages/cards/alaska-visa-signature.js
@@ -1059,7 +1059,7 @@ function AlaskaAirlinesVisaSignatureReviewPage() {
       </main>
         <div className={styles.stickyFooterContainer}>
         <div className={styles.stickyFooterContent}>
-            <Image src={`${siteUrl}${reviewDataNew.imageUrl}`} alt={`${reviewDataNew.cardName} small image`} width={60} height={38} className={styles.stickyFooterCardImage} />
+            <Image src={reviewDataNew.imageUrl} alt={`${reviewDataNew.cardName} small image`} width={60} height={38} className={styles.stickyFooterCardImage} />
             <div className={styles.stickyFooterText}>
               <span className={styles.stickyFooterCardName}>{reviewDataNew.cardName}</span>
               <span className={styles.stickyFooterRating}>{siteName} Rating: {reviewDataNew.ratingValue.toFixed(1)}/10</span>
