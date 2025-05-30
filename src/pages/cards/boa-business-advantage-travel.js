f14585a3b0737277cc8e15f934a9b08fab2369ae
Dilan <dilanmasushanka533@gmail.com>
2025-05-30 22:04:03 +0530

    kjhj

--- src/pages/cards/boa-business-advantage-travel.js
+++ src/pages/cards/boa-business-advantage-travel.js
@@ -715,7 +715,7 @@ function BofABusinessAdvantageTravelRewardsReviewPage() {
                         </div>
                         <div className={styles.summaryBoxActions}>
                             <a href={reviewDataNew.ratesLink} className={styles.summaryRatesLink} target="_blank" rel="noopener noreferrer sponsored">
-                                See Card Rates & Fees (BofA Site)
+                                See Card Rates & Fees
                             </a>
                              <a href='/rewards-compare' className={`${styles.heroRewardsCalculator} ${styles.summaryButton}`} target="_blank" rel="noopener noreferrer"> {/* /* UPDATE THIS LINK if it exists */ }
                                 Business Rewards Calculator
